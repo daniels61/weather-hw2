4a34a8733b68fdcaaed63887df3954bb872093ef
daniel.slotin <daniel.slotin@post.idc.ac.il>
2023-04-27 16:23:22 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Weather from './Weather';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://th.bing.com/th/id/R.50657a76bbfe259776cef53b96004a73?rik=4%2bE1c32iJOCR1A&pid=ImgRaw&r=0' className="App-logo" alt="logo" />
+        <h1><b>Weather news</b></h1>
+        <Weather/>
+        <h2>Daniel Slotin- ID 204300925</h2> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/Weather.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import App from './App';
+
+class Weather extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name,
+      weather: {}
+    };
+  }
+
+  componentDidMount() {
+    const name = this.state.name;
+    const apiKey = '4553536ba9afc39f3c4f1d3d660a5f14';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${apiKey}`
+    axios.get(url)
+      .then(res => {
+        console.log(res);
+        if (res.status === 200) {
+          const weather = res.data;
+          this.setState({ weather });
+        }
+      });
+  }
+
+  handleSearch = () => {
+    const name = this.state.name;
+    const apiKey ='<your API key>';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${apiKey}`
+    axios.get(url)
+      .then(res => {
+        console.log(res);
+        if (res.status === 200) {
+          const weather = res.data;
+          this.setState({ weather });
+        }
+      });
+  }
+
+
+  
+  handleNewSearch = () => {
+    window.location.reload();
+  }
+
+  render() {
+    const { name, weather } = this.state;
+
+    if (weather.main) {
+      return (
+        <>
+          <h1>{name}</h1>
+          <div>{weather.main.temp} °C</div>
+          <div>Feels Like: {weather.main.feels_like} °C</div>
+          <button onClick={this.handleNewSearch}>New Search</button>
+
+        </>
+      );
+    }
+
+    return (
+      <div>
+        <input type="text" value={name}/>
+        <button onClick={this.handleSearch}>Search</button>
+      </div>
+    );
+  }
+}
+
+export default Weather;
